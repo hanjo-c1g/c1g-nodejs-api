2a6a2e14179907760d381efb60970c3c9111dfc1
Hanjo Meinhardt <hanjo@bunix.de>
2025-02-13 15:19:08 +0000

    add solution

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -2,14 +2,22 @@ const todoService = require("../services/todoService");
 
 exports.getTodos = (req, res) => {
     // TODO: Hole alle To-Dos und sende sie als JSON-String zurück
+    res.end(JSON.stringify(todoService.getTodos()));
 };
 
 exports.getTodoById = (req, res, id) => {
     // TODO: Hole das To-Do mit der passenden ID aus dem Service
+    const todo = todoService.getTodoById(id);; // Hier muss die Methode aus todoService genutzt werden
 
     // TODO: Falls kein To-Do gefunden wurde, sende eine 404-Fehlermeldung zurück
+    if (!todo) {
+        res.writeHead(404);
+        res.end("Fehler: To-Do nicht gefunden");
+        return;
+    }
 
     // TODO: Falls das To-Do existiert, sende es als JSON zurück
+    res.end(JSON.stringify(todo));
 };
 
 exports.createTodo = (req, res) => {
@@ -18,10 +26,13 @@ exports.createTodo = (req, res) => {
     req.on("end", () => {
         try {
             // TODO: JSON-Daten parsen
+            const newTodo = JSON.parse(body);
 
             // TODO: To-Do in der Liste speichern
+            const todo = todoService.createTodo(newTodo);
 
             // TODO: Erfolgreiche Antwort senden
+            res.end(JSON.stringify(todo));
         } catch (error) {
             res.end("Invalid Request");
         }
@@ -34,10 +45,16 @@ exports.updateTodo = (req, res, id) => {
     req.on("end", () => {
         try {
             // TODO: JSON-Daten parsen
+            const updatedTodo = JSON.parse(body);
 
             // TODO: To-Do aktualisieren
+            const todo = todoService.updateTodo(id, updatedTodo);
 
-            // TODO: Falls kein To-Do gefunden wurde, sende eine 404-Fehlermeldung zurück, sonst gib das aktualisierte To-Do zurück
+            if (!todo) {
+                res.end("To-Do not found");
+            } else {
+                res.end(JSON.stringify(todo));
+            }
         } catch (error) {
             res.end("Invalid Request");
         }
@@ -46,6 +63,11 @@ exports.updateTodo = (req, res, id) => {
 
 exports.deleteTodo = (req, res, id) => {
     // TODO: Lösche das To-Do mit der übergebenen ID
+    const success = todoService.deleteTodo(id);
 
-    // TODO: Falls kein To-Do gefunden wurde, sende eine 404-Fehlermeldung zurück, sonst ein leeres JSON "{}"
+    if (!success) {
+        res.end("To-Do not found");
+    } else {
+        res.end("To-Do deleted");
+    }
 };
